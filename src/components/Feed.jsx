838e0560db3f1ce7c5a017215265066f9f46439a
ROSHAN REDDY RACHAMALLA <roshanrithik279@gmail.com>
2023-06-01 17:03:40 +0530

    sideBar component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,18 +1,26 @@
 
 import { useState, useEffect } from "react"
 import { Box, Stack, Typography } from '@mui/material'
+import { SideBar, Videos } from "./"
+
 
 const Feed = () => {
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: { sx:0, md:2 }}}>
-        Sidebar
-          
+        <SideBar />
 
-
-          <Typography className="copyright" variant="body2">
-            Copyright 2023
+          <Typography className="copyright" variant="body2" sx={{ mt:1.5, color: '#fff' }}>
+            Copyright 2023 MoonLight
           </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: "90vh", flex: 2}}>
+        <Typography variant='h4' fontWeight="bold" mb={2} sx={{ color: 'white' }} >
+         New <span style={{ color: '#F31503'}}>videos</span>
+        </Typography>
+
+        <Videos videos={[]} />
       </Box>
     </Stack>
   )
